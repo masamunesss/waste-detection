e4523db1a61997f79140b001f32e4a9add5432a5
masamunesss <roy_iot@vips.edu>
2024-04-19 04:18:36 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'wastedetection',
+    version= '0.0.0',
+    author= 'Sachit',
+    author_email= 'sachitstark@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
